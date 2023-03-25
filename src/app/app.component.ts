888a3be0ac4cadd5ce08f9f42aab23bb2ae997a7
Varun Arora <varun.arora@biz2credit.com>
2023-03-25 17:35:52 +0530

    real api urls added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,38 +4,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   totalSolved: number = 0;
   totalQuestions: number = 0;
-  acceptanceRate: number = 0
+  acceptanceRate: number = 0;
   easySolved: number = 0;
-  mediumSolved: number = 0
+  mediumSolved: number = 0;
   hardSolved: number = 0;
-  ranking: 0
+  ranking: 0;
 
-  constructor(private httpClient: HttpClient) {
-  }
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
     // async get request using observables
-    this.httpClient.get('https://leetcode-stats-api.herokuapp.com/').subscribe(
-      (response) => {
-        console.log(response);
-        // this.totalSolved = response.totalSolved;
-        this.totalSolved = response["totalSolved"]
-        this.totalQuestions = response["totalQuestions"]
-        this.easySolved = response["easySolved"]
-        this.mediumSolved = response["mediumSolved"]
-        this.hardSolved = response["hardSolved"]
-        this.acceptanceRate = response["acceptanceRate"]
-        this.ranking = response["ranking"]
-      }, (error) => {
-        console.log(error);
-
-      }
-
-    )
+    this.httpClient
+      .get('https://leetcode-stats-api.herokuapp.com/RDXV')
+      .subscribe(
+        (response) => {
+          console.log(response);
+          // this.totalSolved = response.totalSolved;
+          this.totalSolved = response['totalSolved'];
+          this.totalQuestions = response['totalQuestions'];
+          this.easySolved = response['easySolved'];
+          this.mediumSolved = response['mediumSolved'];
+          this.hardSolved = response['hardSolved'];
+          this.acceptanceRate = response['acceptanceRate'];
+          this.ranking = response['ranking'];
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 }
